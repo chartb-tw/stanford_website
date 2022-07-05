6a5196ad7f3d2d4bc088cdb92d0180b0c63b5d2a
chartb <74368960+chartb-tw@users.noreply.github.com>
2022-07-05 14:55:16 +0100

    assignment2 additions

---
+++ assignment2/bookland.js
@@ -0,0 +1,12 @@
+
+function clearInfo(){
+    let bookform = document.forms["bookland"];
+
+    bookform["textspot"].value = "";
+    bookform["author"].value = "";
+    bookform["booktitle"].value = "";
+}
+
+function findBook(){
+    console.log("Psst, fill this function definition out at some point!");
+}
