7f875082733b2c79eeaf7b1c751fc605ae731e66
chartb <74368960+chartb-tw@users.noreply.github.com>
2022-07-16 12:10:17 +0100

    assignment3 complete

--- assignment3/cardgame.js
+++ assignment3/cardgame.js
@@ -7,16 +7,79 @@ const cardCollection = ["CA", "C2", "C3", "HA", "H2", "H3"];
 
 const cardRound = [...cardCollection];
 
-function resetGame() {
-  for (let i = cardRound.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [cardRound[i], cardRound[j]] = [cardRound[j], cardRound[i]];
+let heldCard = "";
+
+if (document.querySelectorAll(".card-img").length === cardCollection.length) {
+  console.log("Card quantity matches amount of card positions in document");
+} else {
+  console.log("The amount of card positions do not match between HTML and JS");
+}
+
+function hideCardFaces() {
+  const allCards = document.querySelectorAll(".card-img");
+  for (let i = 0; i < allCards.length; i++) {
+    allCards[i].setAttribute("src", "cards/rear.png");
+    allCards[i].setAttribute("onclick", `selectCard(${i})`);
+    allCards[i].classList.remove("hide-card");
   }
-  for (let i = 0; i < 6; i++) {
-    document.querySelectorAll("img")[i].setAttribute("src", "cards/rear.png");
+}
+
+function preventCardClickable() {
+  const allCards = document.querySelectorAll(".card-img");
+  for (let i = 0; i < allCards.length; i++) {
+    allCards[i].setAttribute("onclick", `() => null`);
+  }
+}
+
+function enableCardClickable() {
+  const allCards = document.querySelectorAll(".card-img");
+  for (let i = 0; i < allCards.length; i++) {
+    if (!allCards[i].classList.contains("hide-card")) {
+      allCards[i].setAttribute("onclick", `selectCard(${i})`);
+    }
   }
 }
 
 function flipCard(pos) {
   let clickedCard = document.getElementById(`cardpos${pos}`);
+  clickedCard
+    .querySelector("img")
+    .setAttribute("src", `cards/${cardRound[pos]}.png`);
+}
+
+function resetGame() {
+  for (let i = cardRound.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [cardRound[i], cardRound[j]] = [cardRound[j], cardRound[i]];
+  }
+  hideCardFaces();
+}
+
+function checkCardMatch(card1, card2) {
+  let firstCard = document.getElementById(`cardpos${card1}`);
+  let secondCard = document.getElementById(`cardpos${card2}`);
+  if (cardRound[Number(card1)][1] === cardRound[Number(card2)][1]) {
+    firstCard.querySelector("img").classList.add("hide-card");
+    firstCard.querySelector("img").setAttribute("onclick", `() => ()`);
+    secondCard.querySelector("img").classList.add("hide-card");
+    secondCard.querySelector("img").setAttribute("onclick", `() => ()`);
+  } else {
+    firstCard.querySelector("img").setAttribute("src", "cards/rear.png");
+    secondCard.querySelector("img").setAttribute("src", "cards/rear.png");
+  }
+  enableCardClickable();
+}
+
+function selectCard(pos) {
+  preventCardClickable();
+  if (heldCard === "") {
+    flipCard(pos);
+    heldCard = pos;
+    enableCardClickable();
+  } else if (pos != heldCard) {
+    let test = heldCard;
+    flipCard(pos);
+    setTimeout(() => checkCardMatch(pos, test), 1500);
+    heldCard = "";
+  }
 }
